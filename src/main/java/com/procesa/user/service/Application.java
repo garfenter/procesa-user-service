697973eeddd9298d0b55ac355d6c5946be16f222
garfenter <garfenter@gmail.com>
2017-01-06 09:29:40 -0600

    Add eureka service client

--- src/main/java/com/procesa/user/service/Application.java
+++ src/main/java/com/procesa/user/service/Application.java
@@ -2,12 +2,14 @@ package com.procesa.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *
  * @author Juan Luis Cano <garfenter at adstter.com>
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
     public static void main(String[] args) {
